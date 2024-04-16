2df204cfc0cf6cee08e4c21d3462716fbb081dd5
Rouven Spaar <rouven.spaar@gmail.com>
2024-04-16 08:14:37 +0200

    feat: added themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <ThemeToggle></ThemeToggle>
     </div>
   );
 }
