89a2ff7b364b4c0a07e7c757a248eca75e3ba80f
Rouven Spaar <rouven.spaar@gmail.com>
2024-04-23 08:34:07 +0200

    feat: added card layout in log tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Page } from '@/components/ui/custom/page';
+import styles from './page.module.scss';
 
 export default function Home() {
-  return <Page>Log</Page>;
+  return (
+    <Page>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </Card>
+      <div className={styles.content}>
+        <Card>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Image Preview</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className={styles.imagePreview}></div>
+          </CardContent>
+        </Card>
+      </div>
+    </Page>
+  );
 }
