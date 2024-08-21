0f480136ca155d34ce71d698fc125e5dfd0600ad
Rouven Spaar <rouven.spaar@gmail.com>
2024-08-21 19:10:47 +0200

    feat: added context menu to table

--- src/components/sessionTable/sessionTable.tsx
+++ src/components/sessionTable/sessionTable.tsx
@@ -21,7 +21,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Input } from '../ui/input';
 import {
   DropdownMenu,
@@ -34,6 +34,7 @@ import { Session, useAppState } from '@/context/stateProvider';
 import { columns as imagingSessionColumns } from './columns';
 import { ChevronDown } from 'lucide-react';
 import { UUID } from 'crypto';
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuShortcut, ContextMenuTrigger } from '../ui/context-menu';
 
 export function SessionTable<TData, TValue>({ setSelectedSessionId }: { setSelectedSessionId: (id: UUID) => void }) {
   const { appState } = useAppState();
@@ -48,6 +49,7 @@ export function SessionTable<TData, TValue>({ setSelectedSessionId }: { setSelec
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
   const [selectedRowId, setSelectedRowId] = React.useState<string | undefined>(undefined);
+  const [rowSelected, setRowSelected] = React.useState<boolean>(false);
 
   const table = useReactTable({
     data,
@@ -65,102 +67,128 @@ export function SessionTable<TData, TValue>({ setSelectedSessionId }: { setSelec
     },
   });
 
+  useEffect(() => {
+    setRowSelected(selectedRowId !== undefined);
+  }, [selectedRowId]);
+
   const handleRowClick = (rowId: string) => {
-    setSelectedRowId(rowId);
+    if (rowId === selectedRowId) {
+      setSelectedRowId(undefined);
+    } else {
+      setSelectedRowId(rowId);
+    }
 
     const rowData = table.getRowModel().rows.find((row) => row.id === rowId)?.original as Session;
     setSelectedSessionId(rowData.id);
   };
 
   return (
-    <div className={styles.component}>
-      <div className={styles.header}>
-        <Input
-          placeholder="Search..."
-          value={(table.getColumn('target')?.getFilterValue() as string) ?? ''}
-          onChange={(event) =>
-            table.getColumn('target')?.setFilterValue(event.target.value)
-          }
-          className={styles.searchField}
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns <ChevronDown className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div className={styles.tableWrapper}>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
+    <ContextMenu>
+      <ContextMenuTrigger className='h-full'>
+        <div className={styles.component}>
+          <div className={styles.header}>
+            <Input
+              placeholder="Search..."
+              value={(table.getColumn('target')?.getFilterValue() as string) ?? ''}
+              onChange={(event) =>
+                table.getColumn('target')?.setFilterValue(event.target.value)
+              }
+              className={styles.searchField}
+            />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="ml-auto">
+                  Columns <ChevronDown className="ml-2 h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                {table
+                  .getAllColumns()
+                  .filter((column) => column.getCanHide())
+                  .map((column) => {
+                    return (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className="capitalize"
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
+                      >
+                        {column.id}
+                      </DropdownMenuCheckboxItem>
+                    );
+                  })}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className={styles.tableWrapper}>
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody className='cursor-pointer'>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      onClick={() => handleRowClick(row.id)}
+                      className={selectedRowId === row.id ? styles.selectedRow : ''}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
                           )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  onClick={() => handleRowClick(row.id)}
-                  className={selectedRowId === row.id ? styles.selectedRow : ''}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
                     </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-64">
+        <ContextMenuItem inset>
+          Add new Session...
+          <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset disabled={!rowSelected}>
+          Edit...
+          <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset disabled={!rowSelected}>
+          Delete
+          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 }
