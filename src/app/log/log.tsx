d99cc7967c04a7a11cde2c8fbda69638aae0bf84
Rouven Spaar <rouven.spaar@gmail.com>
2024-08-27 14:52:11 +0200

    feat: added resizable

--- src/app/log/log.tsx
+++ src/app/log/log.tsx
@@ -5,7 +5,7 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle,
+  CardTitle
 } from '@/components/ui/card';
 import { Tab } from '@/components/ui/custom/tab';
 import styles from './log.module.scss';
@@ -17,6 +17,7 @@ import { UUID } from 'crypto';
 import { save } from '@tauri-apps/api/dialog';
 import { toast } from '@/components/ui/use-toast';
 import { invoke } from '@tauri-apps/api/tauri';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 
 export default function Log() {
   const [isModalOpen, setisModalOpen] = useState(false);
@@ -29,7 +30,7 @@ export default function Log() {
 
   function exportCSV() {
     save({
-      defaultPath:"", // TODO: define default path
+      defaultPath: '', // TODO: define default path
       filters: [{
         name: '.csv',
         extensions: ['csv']
@@ -37,14 +38,14 @@ export default function Log() {
     })
       .then((selectedPath) => {
         if (selectedPath) {
-          console.log(selectedPath)
-          invoke('export_csv', { path: selectedPath })
+          console.log(selectedPath);
+          invoke('export_csv', { path: selectedPath });
         }
       })
       .catch((err) => {
         toast({
           variant: 'destructive',
-          description: 'Failed to export CSV: ' + err,
+          description: 'Failed to export CSV: ' + err
         });
         console.log(err);
       });
@@ -63,27 +64,32 @@ export default function Log() {
           <Button variant="secondary" onClick={toggleModal}>
             Add Imaging Session
           </Button>
-          <Button variant='ghost' onClick={exportCSV}>
+          <Button variant="ghost" onClick={exportCSV}>
             Export CSV
           </Button>
         </CardContent>
       </Card>
-      <div className={styles.content}>
-        <Card className={styles.tableCard}>
-          <CardHeader className={styles.tableWrapper}>
-            <SessionTable setSelectedSessionId={setSelectedSessionId} />
-          </CardHeader>
-        </Card>
-        <Card className={styles.imagePreviewCard}>
-          <CardHeader>
-            <CardTitle>Image Preview</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className={styles.imagePreview}></div>
-          </CardContent>
-        </Card>
-      </div>
+      <ResizablePanelGroup className={styles.content} direction="horizontal">
+        <ResizablePanel defaultSize={70}>
+          <Card className={styles.tableCard}>
+            <CardHeader className={styles.tableWrapper}>
+              <SessionTable setSelectedSessionId={setSelectedSessionId} />
+            </CardHeader>
+          </Card>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={30}>
+          <Card className={styles.imagePreviewCard}>
+            <CardHeader>
+              <CardTitle>Image Preview</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className={styles.imagePreview}></div>
+            </CardContent>
+          </Card>
+        </ResizablePanel>
+      </ResizablePanelGroup>
       {isModalOpen && <NewImagingSession onClose={toggleModal} />}
     </Tab>
   );
