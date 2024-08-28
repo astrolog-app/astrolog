5547cbf51c1ad08cbb677a6dda2aa85af53b8e04
Rouven Spaar <rouven.spaar@gmail.com>
2024-08-28 18:14:27 +0200

    feat: added framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import styles from './page.module.scss';
 import { Analytics } from './analytics/analytics';
@@ -7,23 +7,37 @@ import Gallery from './gallery/gallery';
 import { TopBar } from '@/components/topBar';
 import SideNav from '@/components/sideNav';
 import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+export interface Tab {
+  component: React.ReactNode;
+  key: string;
+}
 
 export default function Home() {
-  const tabs = [
-    <Log key="log" />,
-    <Gallery key="gallery" />,
-    <Analytics key="analytics" />
-  ]
-  const [selectedTab, setSelectedTab] = useState<React.ReactNode>(tabs[0]);
+  const tabs: Tab[] = [
+    { component: <Log />, key: 'log' },
+    { component: <Gallery />, key: 'gallery' },
+    { component: <Analytics />, key: 'analytics' }
+  ];
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
   return (
     <div className={styles.tabs}>
       <TopBar />
       <div className={styles.bottom}>
         <SideNav tabs={tabs} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-        <div className={styles.content}>
-          {selectedTab}
-        </div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            className={styles.content}
+            key={selectedTab.key}
+            initial={{ y: -10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+          >
+            {selectedTab.component}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
--- src/components/sideNav.tsx
+++ src/components/sideNav.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import styles from './sideNav.module.scss';
-import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 import React from 'react';
+import { Tab } from '@/app/page';
 
 interface SideNavProps {
-  tabs: React.ReactNode[];
-  selectedTab: React.ReactNode;
-  setSelectedTab: React.Dispatch<React.SetStateAction<React.ReactNode>>;
+  tabs: Tab[];
+  selectedTab: Tab;
+  setSelectedTab: React.Dispatch<React.SetStateAction<Tab>>;
 }
 
 export default function SideNav({ tabs, selectedTab, setSelectedTab }: SideNavProps) {
