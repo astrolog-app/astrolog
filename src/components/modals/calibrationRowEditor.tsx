2400f486c6ada6a8b04f082dc0087a524858319c
Rouven Spaar <rouven.spaar@gmail.com>
2024-09-06 12:53:28 +0200

    update: working formSchema CalibrationRowEditor

--- src/components/modals/calibrationRowEditor.tsx
+++ src/components/modals/calibrationRowEditor.tsx
@@ -1,71 +1,78 @@
+'use client';
+
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Modal } from '@/components/ui/custom/modal';
 import styles from '@/components/modals/calibrationRowEditor.module.scss';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
-import React from 'react';
+import React, { useState } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 import { Input } from '../ui/input';
 import { Label } from '@/components/ui/label';
-import { PopoverContent, PopoverTrigger } from '../ui/popover';
-import { Popover } from '../ui/popover';
-import { CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '../ui/command';
-import { Command } from '../ui/command';
-import { CaretSortIcon, CheckIcon } from '@radix-ui/react-icons';
-import { cn } from '@/utils/classNames';
 import { useModal } from '@/context/modalProvider';
 
-const frameworks = [
-  {
-    value: 'next.js',
-    label: 'Next.js'
-  },
-  {
-    value: 'sveltekit',
-    label: 'SvelteKit'
-  },
-  {
-    value: 'nuxt.js',
-    label: 'Nuxt.js'
-  },
-  {
-    value: 'remix',
-    label: 'Remix'
-  },
-  {
-    value: 'astro',
-    label: 'Astro'
-  }
-];
+enum CalibrationType {
+  DARK = 'DARK',
+  BIAS = 'BIAS'
+}
 
 export default function CalibrationRowEditor() {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState('');
-
   const { closeModal } = useModal();
 
+  const [calibrationType, setCalibrationType] = useState<CalibrationType>(CalibrationType.DARK);
+
   const formSchema = z.object({
-    target: z.array(z.string()).min(1, {
+    camera: z.string().min(1, {
       message: 'You must at least select one calibration frame.'
+    }),
+    calibrationType: z.enum([CalibrationType.DARK, CalibrationType.BIAS], {
+      errorMap: () => ({
+        message: 'You must select a valid calibration type (DARK or BIAS).'
+      })
+    }),
+    gain: z.coerce.number().min(1, {
+      message: 'Gain must be at least 1.'
+    }),
+    subLength: z.coerce.number().optional(),
+    cameraTemp: z.coerce.number().optional(),
+    totalSubs: z.coerce.number().min(1, {
+      message: 'Total subs must be at least 1.'
     })
+  }).superRefine((data, ctx) => {
+    if (data.calibrationType === CalibrationType.DARK) {
+      if (data.subLength === undefined || data.subLength < 1) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'Sub length must be at least 1 for BIAS calibration type',
+          path: ['subLength']
+        });
+      }
+      if (data.cameraTemp === undefined || data.cameraTemp < 1) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'Camera temperature must be at least 1 for BIAS calibration type',
+          path: ['cameraTemp']
+        });
+      }
+    }
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      target: []
+      calibrationType: calibrationType
     }
   });
 
   function onSubmit() {
+    console.log('submit');
   }
 
   return (
     <Modal
       title="Add Calibration Frames"
-      separator
       className={styles.modal}
     >
       <Form {...form}>
@@ -74,53 +81,11 @@ export default function CalibrationRowEditor() {
             <Label className={styles.label}>Camera</Label>
             <FormField
               control={form.control}
-              name="target"
+              name="camera"
               render={({ field }) => (
                 <FormItem className={styles.item}>
                   <FormControl>
-                    <Popover open={open} onOpenChange={setOpen}>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant="outline"
-                          role="combobox"
-                          aria-expanded={open}
-                          className="w-[200px] justify-between"
-                        >
-                          {value
-                            ? frameworks.find((framework) => framework.value === value)?.label
-                            : 'Select framework...'}
-                          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-[200px] p-0">
-                        <Command>
-                          <CommandInput placeholder="Search framework..." className="h-9" />
-                          <CommandList>
-                            <CommandEmpty>No framework found.</CommandEmpty>
-                            <CommandGroup>
-                              {frameworks.map((framework) => (
-                                <CommandItem
-                                  key={framework.value}
-                                  value={framework.value}
-                                  onSelect={(currentValue) => {
-                                    setValue(currentValue === value ? '' : currentValue);
-                                    setOpen(false);
-                                  }}
-                                >
-                                  {framework.label}
-                                  <CheckIcon
-                                    className={cn(
-                                      'ml-auto h-4 w-4',
-                                      value === framework.value ? 'opacity-100' : 'opacity-0'
-                                    )}
-                                  />
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </CommandList>
-                        </Command>
-                      </PopoverContent>
-                    </Popover>
+                    <Input {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -131,18 +96,23 @@ export default function CalibrationRowEditor() {
             <Label className={styles.label}>Calibration Type</Label>
             <FormField
               control={form.control}
-              name="target"
+              name="calibrationType"
               render={({ field }) => (
                 <FormItem className={styles.item}>
                   <FormControl>
-                    <Select>
+                    <Select
+                      value={field.value}
+                      onValueChange={(value) => {
+                        setCalibrationType(CalibrationType[value as keyof typeof CalibrationType]);
+                        field.onChange(value);
+                      }}
+                    >
                       <SelectTrigger>
-                        <SelectValue placeholder="Select camera" />
+                        <SelectValue placeholder="Select calibration type" />
                       </SelectTrigger>
                       <SelectContent>
-                        <SelectItem value="camera1">Camera 1</SelectItem>
-                        <SelectItem value="camera2">Camera 2</SelectItem>
-                        <SelectItem value="camera3">Camera 3</SelectItem>
+                        <SelectItem value="DARK">DARK</SelectItem>
+                        <SelectItem value="BIAS">BIAS</SelectItem>
                       </SelectContent>
                     </Select>
                   </FormControl>
@@ -155,56 +125,60 @@ export default function CalibrationRowEditor() {
             <Label className={styles.label}>Gain</Label>
             <FormField
               control={form.control}
-              name="target"
+              name="gain"
               render={({ field }) => (
                 <FormItem className={styles.item}>
                   <FormControl>
-                    <Input type="number" />
+                    <Input type="number" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
           </div>
-          <div className={styles.row}>
-            <Label className={styles.label}>Sub Length</Label>
-            <FormField
-              control={form.control}
-              name="target"
-              render={({ field }) => (
-                <FormItem className={styles.item}>
-                  <FormControl>
-                    <Input type="number" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className={styles.row}>
-            <Label className={styles.label}>Camera Temp.</Label>
-            <FormField
-              control={form.control}
-              name="target"
-              render={({ field }) => (
-                <FormItem className={styles.item}>
-                  <FormControl>
-                    <Input type="number" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+          {calibrationType === CalibrationType.DARK && (
+            <>
+              <div className={styles.row}>
+                <Label className={styles.label}>Sub Length</Label>
+                <FormField
+                  control={form.control}
+                  name="subLength"
+                  render={({ field }) => (
+                    <FormItem className={styles.item}>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className={styles.row}>
+                <Label className={styles.label}>Camera Temp.</Label>
+                <FormField
+                  control={form.control}
+                  name="cameraTemp"
+                  render={({ field }) => (
+                    <FormItem className={styles.item}>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </>
+          )}
           <div className={styles.row}>
             <Label className={styles.label}>Total Subs</Label>
             <FormField
               control={form.control}
-              name="target"
+              name="totalSubs"
               render={({ field }) => (
                 <FormItem className={styles.item}>
                   <FormControl>
-                    <Input type="number" />
+                    <Input type="number" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
