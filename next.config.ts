f093db7c05afffde9df58b535a16f8a34c8b777f
Rouven Spaar <rouven.spaar@gmail.com>
2026-06-09 11:54:34 +0200

    initial tauri setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Ensure Next.js uses SSG instead of SSR
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: 'export',
+  // Note: This feature is required to use the Next.js Image component in SSG mode.
+  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+  images: {
+    unoptimized: true,
+  },
+  // Configure assetPrefix or else the server won't properly resolve your assets.
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
